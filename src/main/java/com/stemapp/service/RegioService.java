d6f38cb4434ea9e2fc3a2ad24be9eb96a9136e5f
JamieKalloe <Jamie@Kalloe.net>
2016-03-27 14:37:57 +0100

    adjusted .gitignore and finished RegioService

--- src/main/java/com/stemapp/service/RegioService.java
+++ src/main/java/com/stemapp/service/RegioService.java
@@ -3,6 +3,8 @@ package com.stemapp.service;
 import com.stemapp.model.Regio;
 import com.stemapp.persistence.RegioDAO;
 
+import java.util.Collection;
+
 /**
  * Created by Jamie on 27-3-2016.
  */
@@ -13,5 +15,31 @@ public class RegioService extends BaseService<Regio>{
 
     public RegioService(RegioDAO dao) {
         this.dao = dao;
+    }
+
+    public Collection<Regio> getAll() {
+        return dao.getAll();
+    }
+
+    public Regio get(int id) {
+        return requireResult(dao.get(id));
+    }
+
+    public void add(Regio regio) {
+        dao.add(regio);
+    }
+
+    public void update(int id, Regio regio) {
+        //Check if it exists
+        Regio checkRegio = get(id);
+        
+        dao.update(id, regio);
+    }
+
+    public void delete(int id) {
+        //Check if it exists
+        Regio checkRegio = get(id);
+
+        dao.delete(id);
     }
 }
