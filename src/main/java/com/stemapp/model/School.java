1ddbfd32c9a1e85873f4891625e05f735c548df7
JamieKalloe <Jamie@Kalloe.net>
2016-03-28 13:09:09 +0100

    added School model

--- src/main/java/com/stemapp/model/School.java
+++ src/main/java/com/stemapp/model/School.java
@@ -1,7 +1,46 @@
 package com.stemapp.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.stemapp.View;
+import org.hibernate.validator.constraints.NotEmpty;
+
 /**
  * Created by Jamie on 28-3-2016.
  */
 public class School {
+
+    //Variables
+    @NotEmpty
+    @JsonView(View.Public.class)
+    private String name;
+
+    @JsonView(View.Public.class)
+    private Regio regio;
+
+    @JsonView(View.Private.class)
+    private int id;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Regio getRegio() {
+        return this.regio;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRegio(Regio regio) {
+        this.regio = regio;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
