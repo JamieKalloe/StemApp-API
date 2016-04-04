28986387cbbd2c47f46310fd4c0d6cd70b585621
JamieKalloe <Jamie@Kalloe.net>
2016-04-04 20:36:04 +0100

    added Stelling model

---
+++ src/main/java/com/stemapp/model/Stelling.java
@@ -0,0 +1,64 @@
+package com.stemapp.model;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.stemapp.View;
+
+/**
+ * Created by Jamie on 4-4-2016.
+ */
+public class Stelling {
+
+    @JsonView(View.Public.class)
+    private int id, vragenlijstId;
+
+    @JsonView(View.Public.class)
+    private String titel, stelling, uitleg;
+
+    public Stelling() {
+
+    }
+
+    public Stelling(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    private int getVragenlijstId() {
+        return this.vragenlijstId;
+    }
+
+    public String getTitel() {
+        return this.titel;
+    }
+
+    public String getStelling() {
+        return this.stelling;
+    }
+
+    public String getUitleg() {
+        return this.uitleg;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setVragenlijstId(int vragenlijstId) {
+        this.vragenlijstId = vragenlijstId;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public void setStelling(String stelling) {
+        this.stelling = stelling;
+    }
+
+    public void setUitleg(String uitleg) {
+        this.uitleg = uitleg;
+    }
+}
