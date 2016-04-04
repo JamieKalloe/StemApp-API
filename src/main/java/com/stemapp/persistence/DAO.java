da0307ea0968a41e614eea0361ab690d800921be
JamieKalloe <Jamie@Kalloe.net>
2016-04-04 17:06:55 +0100

    test interface

---
+++ src/main/java/com/stemapp/persistence/DAO.java
@@ -0,0 +1,12 @@
+package com.stemapp.persistence;
+
+import java.util.List;
+
+/**
+ * Created by Jamie on 4-4-2016.
+ */
+public interface DAO {
+
+//    Object get(int id);
+    void add(Object object);
+}
---
+++ src/main/java/com/stemapp/persistence/VragenlijstDAO.java
@@ -0,0 +1,7 @@
+package com.stemapp.persistence;
+
+/**
+ * Created by Jamie on 4-4-2016.
+ */
+public class VragenlijstDAO {
+}
