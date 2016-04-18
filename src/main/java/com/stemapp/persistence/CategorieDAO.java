f08986fa2a854a068fbb38a4ccff8da1dca36065
JamieKalloe <Jamie@Kalloe.net>
2016-04-18 22:04:35 +0100

    updated and cleaned up CategorieDAO (SQL, no more arraylists)

--- src/main/java/com/stemapp/persistence/CategorieDAO.java
+++ src/main/java/com/stemapp/persistence/CategorieDAO.java
@@ -16,56 +16,35 @@ import java.util.List;
 public class CategorieDAO implements DAO<Categorie> {
 
     //Variables
-    private List<Categorie> categories;
     private final Database databaseInstance;
 
     public CategorieDAO() {
         this.databaseInstance = Database.getInstance();
-        this.categories = this.getAllFromDatabase();
     }
 
     @Override
     public List<Categorie> getAll() {
-        return this.categories;
+        return this.getAllFromDatabase();
     }
 
     @Override
     public Categorie get(int id) {
-        try {
-            for(Categorie categorie : categories) {
-                if(categorie.getId() == id) {
-                    return categorie;
-                }
-            }
-            return null;
-
-        } catch (IndexOutOfBoundsException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return this.getCategorie(id);
     }
 
     @Override
     public void add(Categorie categorie) {
-        categorie = this.addCategorieToDatabase(categorie);
-        categories.add(categorie);
+        this.addCategorieToDatabase(categorie);
     }
 
     @Override
     public void update(int id, Categorie categorie) {
-        Categorie oldCategorie = this.get(id);
-        categorie.setId(id);
-
-        this.updateCategorieFromDatabase(categorie);
-        int idInList = categories.indexOf(oldCategorie);
-        categories.set(idInList, categorie);
+        this.updateCategorieFromDatabase(id, categorie);
     }
 
     @Override
     public void delete(int id) {
-        Categorie categorie = this.get(id);
-        this.removeCategorieFromDatabase(categorie);
-        categories.remove(categorie);
+        this.removeCategorieFromDatabase(get(id));
     }
 
     private List<Categorie> getAllFromDatabase() {
@@ -89,6 +68,23 @@ public class CategorieDAO implements DAO<Categorie> {
         return categorieList;
     }
 
+    public Categorie getCategorie(int id) {
+        ResultSet results = databaseInstance.select("categorie", id);
+        Categorie categorie = new Categorie();
+
+        try {
+            if(results.next()) {
+                categorie.setId(results.getInt("id"));
+                categorie.setName(results.getString("naam"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return categorie;
+    }
+
     private Categorie addCategorieToDatabase(Categorie categorie) {
         HashMap databaseData = new HashMap();
 
@@ -100,12 +96,12 @@ public class CategorieDAO implements DAO<Categorie> {
         return categorie;
     }
 
-    private void updateCategorieFromDatabase(Categorie categorie) {
+    private void updateCategorieFromDatabase(int id, Categorie categorie) {
         HashMap databaseData = new HashMap();
 
         databaseData.put("naam", categorie.getName());
 
-        databaseInstance.update("categorie", categorie.getId(), databaseData);
+        databaseInstance.update("categorie", id, databaseData);
     }
 
     private void removeCategorieFromDatabase(Categorie categorie) {
